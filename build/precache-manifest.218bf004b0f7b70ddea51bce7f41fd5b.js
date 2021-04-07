17b791323f6a86620297e38c18b86ba8f3cf4e7b
Shulha Nikita <shulgamalthael@gmail.com>
2021-04-07 18:07:01 +0300

    init files

---
+++ build/precache-manifest.218bf004b0f7b70ddea51bce7f41fd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb22f1cd41ef9ce566a1760ae37b7963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e990b15ad311d878a0fe",
+    "url": "/static/css/main.32d66eb0.chunk.css"
+  },
+  {
+    "revision": "b7e9b1400e663de2ba80",
+    "url": "/static/js/2.1a9df111.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/static/js/2.1a9df111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e990b15ad311d878a0fe",
+    "url": "/static/js/main.4e453210.chunk.js"
+  },
+  {
+    "revision": "0f9476f69e2731bbc064",
+    "url": "/static/js/runtime-main.91bdc04b.js"
+  }
+]);
